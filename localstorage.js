47267510bcbb5d614e58e76e4880da9979f759d7
b0wter <b0wter@gmxd.de>
2020-04-04 23:23:58 +0200

    Removed return values from js functions since they do nothing.
    Elm -> Js is one-way only.

--- localstorage.js
+++ localstorage.js
@@ -1,11 +1,11 @@
 function saveToLocalStorage(key, value) {
+    // Messages from Elm to JS are one-way only!
+    // There is no point in defining a return value.
     try {
         localStorage.setItem(key, value);
-        return true;
     }
     catch(ex) {
-        console.log(ex);
-        return false;
+        console.error(`There was an error trying to save '${key}' - '${value}' to the local storage:`, ex);
     }
 }
 
@@ -14,12 +14,12 @@ function loadFromLocalStorage(key) {
 }
 
 function removeFromLocalStorage(key) {
+    // Messages from Elm to JS are one-way only!
+    // There is no point in defining a return value.
     try {
         localStorage.removeItem(key);
-        return true;
     }
     catch(ex) {
         console.log(ex);
-        return false;
     }
 }
